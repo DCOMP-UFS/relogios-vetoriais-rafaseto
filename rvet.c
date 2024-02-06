053cb0e713b13b8b914ce3adebd4da587e0f5896
abUFS <132862745+abUFS@users.noreply.github.com>
2024-02-06 12:23:54 -0300

    1a tentativa
    1a Tentativa.
    - Artur

--- rvet.c
+++ rvet.c
@@ -18,45 +18,71 @@ typedef struct Clock {
    int p[3];
 } Clock;
 
+void Clock_logging(int pid, Clock *clock){
+   printf("Process: %d, Clock: (%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]);
+}
 
-void Event(int pid, Clock *clock){
-   clock->p[pid]++;   
+void Event(int pid, Clock *clock, int logg){
+   clock->p[pid]++;
+   if (logg) {Clock_logging(pid, clock);}
 }
 
 
-void Send(int pid, Clock *clock){
-   // TO DO
+void Send(int pid_send_to, Clock *clock, int pid_sender){
+   Event(pid_sender, clock, 0);
+   MPI_Send(&clock->p[0], 3, MPI_INT, pid_send_to, 0, MPI_COMM_WORLD);
+   Clock_logging(pid_sender, clock);
 }
 
-void Receive(int pid, Clock *clock){
-   // TO DO
-
+void Receive(int pid_receive_from, Clock *clock, int pid_receiver){
+   Clock temp_clock = {{0,0,0}};
+   MPI_Status status;
+   
+   Event(pid_receiver, clock, 0);
+   
+   MPI_Recv(&temp_clock.p[0], 3, MPI_INT, pid_receive_from, 0, MPI_COMM_WORLD, &status);
+   //Atualizar o relogio interno comparando com o relogio recebido
+   for (int i = 0; i < 3; i++){
+      clock->p[i] = (temp_clock.p[i] > clock->p[i]) ? temp_clock.p[i] : clock->p[i];
+   }
+   Clock_logging(pid_receiver, clock);
 }
+
 
 // Representa o processo de rank 0
 void process0(){
    Clock clock = {{0,0,0}};
-   Event(0, &clock);
-   printf("Process: %d, Clock: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
-
-   // TO DO
-
+   
+   int my_id = 0;
+   
+   Event(my_id, &clock, 1);
+   Send(1, &clock, my_id);
+   Receive(1, &clock, my_id);
+   Send(2, &clock, my_id);
+   Receive(2, &clock, my_id);
+   Send(1, &clock, my_id);
+   Event(my_id, &clock, 1);
+   
 }
 
 // Representa o processo de rank 1
 void process1(){
    Clock clock = {{0,0,0}};
-   printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
-
-   // TO DO
+   int my_id = 1;
+   
+   Send(0, &clock, my_id);
+   Receive(0, &clock, my_id);
+   Receive(0, &clock, my_id);
 }
 
 // Representa o processo de rank 2
 void process2(){
    Clock clock = {{0,0,0}};
-   printf("Process: %d, Clock: (%d, %d, %d)\n", 2, clock.p[0], clock.p[1], clock.p[2]);
+   int my_id = 2;
    
-   // TO DO
+   Event(my_id, &clock, 1);
+   Send(0, &clock, my_id);
+   Receive(0, &clock, my_id);
 }
 
 int main(void) {
